c0a0fe4d52282ebeb92b53b98121693ada440823
Álvaro Medeiros <alvaro.medeiros@hp.com>
2022-05-18 18:41:21 -0300

    learning how to use snapshots

--- src/cart/Cart.spec.js
+++ src/cart/Cart.spec.js
@@ -18,60 +18,78 @@ describe('Cart', () => {
     };
   });
 
-  it('return 0 when the method getTotal() is called in a newly created instance', () => {
-    expect(cart.getTotal()).toEqual(0);
+  describe('getTotal()', () => {
+    it('return 0 when the method getTotal() is called in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('should multiply quantity and price, and receive the total amount', () => {
+      cart.add({
+        product,
+        quantity: 4, // 59.960
+      });
+
+      expect(cart.getTotal()).toEqual(59960);
+    });
+
+    it('should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      cart.add({
+        product,
+        quantity: 4, // 59.960
+      });
+
+      expect(cart.getTotal()).toEqual(59960);
+    });
+
+    it('should update the quantity of a product that exists on cart when added the same product', () => {
+      cart.add2({
+        product,
+        quantity: 2,
+      });
+
+      cart.add2({
+        product,
+        quantity: 4, // 59.960
+      });
+
+      expect(cart.getTotal()).toEqual(89940);
+    });
+
+    it('should update the total when a product gets include or removed', () => {
+      cart.add({
+        product,
+        quantity: 2, //29.980
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 4, // 131.960
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(131960);
+    });
   });
 
-  it('should multiply quantity and price, and receive the total amount', () => {
-    cart.add({
-      product,
-      quantity: 4, // 59.960
+  describe('Name of the group', () => {
+    it('should return an object with the total and the list of itens of cart', () => {
+      cart.add({
+        product,
+        quantity: 54,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 48,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
     });
-
-    expect(cart.getTotal()).toEqual(59960);
-  });
-
-  it('should ensure no more than on product exists at a time', () => {
-    cart.add({
-      product,
-      quantity: 1,
-    });
-
-    cart.add({
-      product,
-      quantity: 4, // 59.960
-    });
-
-    expect(cart.getTotal()).toEqual(59960);
-  });
-
-  it('should update the quantity of a product that exists on cart when added the same product', () => {
-    cart.add2({
-      product,
-      quantity: 2,
-    });
-
-    cart.add2({
-      product,
-      quantity: 4, // 59.960
-    });
-
-    expect(cart.getTotal()).toEqual(89940);
-  });
-
-  it('should update the total when a product gets include or removed', () => {
-    cart.add({
-      product,
-      quantity: 2, //29.980
-    });
-
-    cart.add({
-      product: product2,
-      quantity: 4, // 131.960
-    });
-
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(131960);
   });
 });
