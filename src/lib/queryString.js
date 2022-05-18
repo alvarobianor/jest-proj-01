dff5482194f72f35ba6d02b0de0be63d1128fd01
Álvaro Medeiros <alvaro.medeiros@hp.com>
2022-05-18 11:34:05 -0300

    separate two logics

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,12 +1,12 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please, check your params');
+  }
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = (obj) =>
-  Object.entries(obj)
-    .map(([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw new Error('Please, check your params');
-      }
-      return `${key}=${value}`;
-    })
-    .join('&');
+  Object.entries(obj).map(keyValueToString).join('&');
 
 // module.exports.objectToQueryString = (obj) =>
 //   Object.entries(obj)
